183d7027a05cb9738733b0bd92edf6eedb65bb01
jaime perdue <jperdue@ucsd.edu>
2018-03-01 10:44:32 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,38 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+
+$(function(){
+	$(".judge-img").bind("taphold", tapholdHandler );
+
+		function tapholdHandler(event){
+			var targetIDPrefex = event.target.id;
+			$("#" + targetIDPrefex + "-bio").show();
+		}
+});
+
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function(e) {
+		e.preventDefault();
+		var rsvpEmail = $('#rsvpEmail').val();
+
+
+		$.post('addRSVP', {	rsvpEmail: rsvpEmail }, postCallback);
+	});
+
+	function postCallback(res) {
+		alert("rsvp submitted");
+		$('#rsvpEmail').val('');
+	}
 }
+
+
+$(function(){
+	$("div.box").bind("taphold", tapholdHandler );
+	function tapholdHandler(event){
+		$(event.target).addClass ("taphold");
+	}
+});
